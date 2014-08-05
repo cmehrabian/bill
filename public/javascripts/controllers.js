a6388d6649a220b8b6919b1991565c8a8b58b4ea
Tyler Smith <tylsmith@gmail.com>
2014-08-04 21:38:11 -0700

    literally just put semicolons on things

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,49 +1,49 @@
 angular.module('controllers', [])
 .controller('graphCtrl', function($scope, graph, $routeParams, socket, $http){
 
-	graph.init()
+	graph.init();
 
 	$scope.$on('$destroy', function(){
-		socket.removeAllListeners()
+		socket.removeAllListeners();
 	})
 
-	$scope.selected = null
-	$scope.children = null
-	$scope.parent = null
+	$scope.selected = null;
+	$scope.children = null;
+	$scope.parent = null;
 
-	$scope.username = ''
-	$scope.flavor = 'comment'
-	$scope.text = ''
-	$scope.links = []
-	$scope.editing = false
-	$scope.looking = true
+	$scope.username = '';
+	$scope.flavor = 'comment';
+	$scope.text = '';
+	$scope.links = [];
+	$scope.editing = false;
+	$scope.looking = true;
 
-	$scope.flavors = ['comment', 'assent', 'dissent', 'quote']
+	$scope.flavors = ['comment', 'assent', 'dissent', 'quote'];
 
 	var updateSelected = function(selected, children, parent){
-		$scope.selected = selected
-		$scope.children = children
-		$scope.parent = parent
-		$scope.looking = true
-		if(!$scope.$$phase) {
-			$scope.$digest()
+		$scope.selected = selected;
+		$scope.children = children;
+		$scope.parent = parent;
+		$scope.looking = true;
+		if(!$scope.$$phase) {;
+			$scope.$digest();
 		}	
 	}
 
-	graph.registerSelectionChange(updateSelected)
+	graph.registerSelectionChange(updateSelected);
 
 	$scope.select = function(node){
 		if(node == null || node === undefined)
-			return
-		graph.select(node)
+			return;
+		graph.select(node);
 	}
 
 	$scope.close = function(){
-		$scope.looking = false
+		$scope.looking = false;
 	}
 
 	$scope.cancel = function(){
-		$scope.editing = false
+		$scope.editing = false;
 	}
 
 	socket.emit('request', {
@@ -51,17 +51,17 @@ angular.module('controllers', [])
 	})
 
   	socket.on('update', function (data) {
-  		graph.update(data, $routeParams.point_id)
+  		graph.update(data, $routeParams.point_id);
 	});
 
   	$scope.reset = function(){
-  		console.log($scope.selected)
+  		console.log($scope.selected);
   	}
 
 	$scope.submit = function(){
 
 		if($scope.selected == null)
-			return
+			return;
 
 		var n = {
 			username:$scope.username,
@@ -78,19 +78,19 @@ angular.module('controllers', [])
 			propagated:0
 		}
 
-		$scope.flavor = 'comment'
-		$scope.text = ''
+		$scope.flavor = 'comment';
+		$scope.text = '';
 
 		// we don't really care about the response.
-		$http({method: 'POST', url: '/submit', data: n})
+		$http({method: 'POST', url: '/submit', data: n});
 
-		$scope.editing = false
+		$scope.editing = false;
 	}
 })
 
 .controller('newCtrl', function($scope, $location, $http){
-	$scope.username = ''
-	$scope.text = ''
+	$scope.username = '';
+	$scope.text = '';
 
 
 	$scope.submit = function(){
@@ -121,7 +121,6 @@ angular.module('controllers', [])
 		    	console.log(headers);
 		    	console.log(config);
 		    });
-
 	}
 
 })
@@ -129,7 +128,7 @@ angular.module('controllers', [])
 .controller('boardCtrl', function($scope, $routeParams, $http){
   $http({method: 'GET', url: '/requestTopics'}).
     success(function(data, status, headers, config) {
-    	$scope.topics = data
+    	$scope.topics = data;
     }).
     error(function(data, status, headers, config) {
     	console.log(data);
