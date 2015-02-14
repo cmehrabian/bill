df139bc1406bd6245aab35407b651fe2b3515459
Tyler Smith <tylsmith@gmail.com>
2015-02-13 17:51:57 -0800

    (large amount of unfinished preference code)

--- app/client/user/user.js
+++ app/client/user/user.js
@@ -3,6 +3,18 @@ Template.user.user = function () {
 	return Meteor.users.findOne({username: Router.current().params._username});
 }
 
+Template.user.isViewingOwnPage = function () {
+	return Meteor.user() && Meteor.user().username == Router.current().params._username;
+}
+
+Template.user.email = function () {
+	var emails = Template.user.user().emails;
+	if (emails)
+		return emails[0].address;
+	else
+		return "email address not registered!";
+}
+
 Template.user.posts = function () {
 	return Nodes.find({username: Router.current().params._username},
 	 {sort:{timestamp:-1}}
