5dc4d48edf6376a2fa6ea451731529877c13de3e
Tyler Smith <tylsmith@gmail.com>
2015-02-26 20:26:29 -0800

    newpie html mockup

--- app/client/newpie/newpie.js
+++ app/client/newpie/newpie.js
@@ -1,14 +1,11 @@
 Template.newpie.helpers({
   isLoggedIn: function() {
-    console.log("test")
     return !!Meteor.user();
   },
   loggedInUsername: function() {
     return Meteor.user().username;
   },
   userList: function () {
-    console.log("asdfasdf")
-    console.log( Meteor.users.find().fetch());
-    return [];
+    return Meteor.users.find().fetch();
   }
 });
