63e176c21e470e2d325e2f080c9e54d3ca576ec4
Tyler Smith <tylsmith@gmail.com>
2015-03-09 17:17:01 -0700

    chat log implemented

---
+++ app/client/chat/chat.js
@@ -0,0 +1,20 @@
+Template.chat.helpers({
+  chatLog:function(){
+    return Chatter.find({}, {$sort:{timestamp:1}});
+  }
+});
+
+Template.chat.events({
+    'keypress input': function(e) {
+      if(e.keyCode != 13)
+        return;
+
+      var message = document.getElementById("chat-box").value;
+
+      if(message.length == 0)
+        return;
+
+      Meteor.call("newMessage", message);
+      document.getElementById("chat-box").value = "";
+    }
+});
