05d402754a29c85b2fbd97ade5d2234abf2758c7
Tyler Smith <tylsmith@gmail.com>
2015-03-17 22:29:29 -0700

    now displays when a post was last updated

--- app/client/view/forumview/forumviewPost.js
+++ app/client/view/forumview/forumviewPost.js
@@ -7,33 +7,6 @@ Template.forumviewPost.helpers({
       return "red";
     return "black";
   },
-  time: function(timestamp){
-    var now = Date.now();
-    var seconds = Math.floor((now - timestamp) / 1000);
-
-    var interval = Math.floor(seconds / 31536000);
-
-    if (interval > 1) {
-        return interval + " years ago";
-    }
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-        return interval + " months ago";
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-        return interval + " days ago";
-    }
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-        return interval + " hours ago";
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-        return interval + " minutes ago";
-    }
-    return Math.floor(seconds) + " seconds ago";
-  },
   isCommenting: function(_id){
     return !! _.find(Session.get("isCommenting"), function(c){
       return c == _id;
